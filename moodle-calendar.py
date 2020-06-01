58d01e4b5418842b8d9959bf27b031de9ef2ff81
bakatronium <lukario.maverick@gmail.com>
2020-06-01 20:34:20 -0300

    Finallized version 1.0. Added configuration file.

--- moodle-calendar.py
+++ moodle-calendar.py
@@ -1,52 +1,45 @@
+import time
+start_time = time.time()
+print("Initiating activation sequence.")
+from configparser import RawConfigParser
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-start_time = time.time()
+import os
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-print("Initiating...")
-print("Welcome to moodle for lazy people version 0.12, developed by Luchas. Edit the link of lists to add or remove any specific subjects. Does not support OCR, so it won't detected lessons not properly set by teachers.")
+print("Reading configuration file.")
+filename = "config.cfg"
+if os.path.isfile(filename):
+    parser = RawConfigParser()
+    parser.read(filename)
+else:
+    print("Config file not found")
 
+print("-----------")
+print("Welcome to moodle for lazy people version 0.12, developed by Luchas.") 
+print("Edit the config.cfg file to add or remove any specific subjects. Does not support lessons not properly set by teachers.")
 print("Please Input your username")
 username = input()
 print("Please Input your password")
 password = input()
-
 print("-----------")
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
 print("Setting up links.")
-mainlogin = "https://ead.fecap.br/moodleFECAPCol/" 
-algebra = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4139"
-biologia = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4140"
-edfisica = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4141"
-empreend = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4192"
-filosofia = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4142"
-fisica = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4143"
-geografia = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4144"
-geometria = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4145"
-gramatica = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4146"
-historia = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4147"
-prodtexto = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4148"
-literatura = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4149"
-quimica = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4150"
-sociologia = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4151"
-#ingles = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=4278"
-#tecnico1 = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=3703"
-#tecnico2 = "https://ead.fecap.br/moodleFECAPCol/course/view.php?id=3704"
-
-links = [algebra, biologia, edfisica, empreend, filosofia, fisica, geografia, geometria, gramatica, historia, prodtexto, literatura, quimica, sociologia]
+mainlink = parser.get('links', 'mainlink')
+subjects = parser.get('links', 'subjects')
+links = subjects.split("\n")
+links.pop(0)
+print(links)
 
 print("Logging in...")
-driver.get(mainlogin)
+driver.get(mainlink)
 loginform = driver.find_element_by_id("login_username")
 loginform.send_keys(username)
 passwordform = driver.find_element_by_id("login_password")
 passwordform.send_keys(password)
 passwordform.send_keys(Keys.RETURN)
 
-print("Setting up variables...")
 assigned = list()
 quizzed = list()
 
@@ -59,7 +52,6 @@ def both_check(subject):
     quiz = driver.find_elements_by_css_selector("[href*='/quiz']")
     for x in range(len(quiz)):
         var5 = quiz[x].get_attribute('href')
-        quizzed.append(var5)
 
 def assignment_date_check():
     for x in range(len(assigned)):
@@ -77,6 +69,7 @@ def assignment_date_check():
                     pass
                 else:
                     print("-----------")
+                    print("Type: Quiz")
                     print("Link: " + href)
                     print("Subject: " + subject)
                     print("Title: " + title)
@@ -90,15 +83,16 @@ def quiz_date_check():
         titlebox = driver.find_element_by_xpath("//*[@id='region-main']/div/div[1]").text
         if "fechado" in titlebox:
             print("-----------")
+            print("Type: Quiz")
             print("Link: " + href)
             print("Subject: " + subject)
             print("Information: " + titlebox)
 
 for x in range(len(links)):
     both_check(links[x])
-
+print("Following assignments found:")
 assignment_date_check()
 quiz_date_check()
-print("Done.")
+print("Moodle Calendar finalized..")
 print("Total Runtime:")
 print("--- %s seconds ---" % (time.time() - start_time))
